f839b49effbd2b0437100749b7cb529b92c300e1
Hung The Nguyen <hungn2@stanford.edu>
2018-06-07 05:56:09 -0700

    fixes

--- DataX/js/main.js
+++ DataX/js/main.js
@@ -54,8 +54,10 @@
 
 })(jQuery);
 
+var profile;
+
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
+    profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
