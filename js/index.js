5a9e0ace9ac08141188ed81af0a9287ea9a60dd1
Hung The Nguyen <hungnguyen094@gmail.com>
2018-10-27 03:12:31 -0700

    Minor setup changes
    Minor changes to setup the color scheme, the font, and the menu items using js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+class App {
+  constructor() {
+    const menu = document.querySelector('#menu');
+    console.log(menu);
+    // This.thing = new class(init, init2);
+  }
+}
+
+const app = new App();
