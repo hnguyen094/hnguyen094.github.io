a1963dd7ca2dd3afd6d489d6a4baa395a47015ae
Hung The Nguyen <hungnguyen094@gmail.com>
2018-10-28 06:08:48 -0700

    Basic page loading
    Finished basic page loading mechanics, whilst watching the new Daredevil

--- js/index.js
+++ js/index.js
@@ -1,8 +1,31 @@
 class App {
   constructor() {
-    const menu = document.querySelector('#menu');
-    console.log(menu);
+    const menu = document.querySelectorAll('.menu-item');
+    for (let menu_item of menu) {
+      menu_item.addEventListener("mouseover", () => {
+        menu_item.classList.add('hover');
+        menu_item.classList.remove('unhover');
+      });
+      menu_item.addEventListener("mouseout", () => {
+        menu_item.classList.remove('hover');
+        menu_item.classList.add('unhover');
+      });
+      menu_item.addEventListener("click", () => {
+        for (let item of menu) {
+          item.classList.add('unselected');
+          item.classList.remove('selected');
+        }
+        menu_item.classList.add('selected');
+        menu_item.classList.remove('unselected');
+        this.openPage();
+      });
+    }
     // This.thing = new class(init, init2);
+  }
+  openPage() {
+    let content = document.querySelector('#content');
+    content.querySelector('h1').innerHTML = "potatoes";
+    content.querySelector('span').innerHTML = "<p>text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum text lorum ipsum</p>";
   }
 }
 
