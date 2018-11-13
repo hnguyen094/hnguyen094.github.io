611fa308076915a87a8d4e360639eb4b096cad3a
Hung Nguyen <hungnguyen094@gmail.com>
2018-11-12 20:50:11 -0800

    updated text format

--- js/content.js
+++ js/content.js
@@ -1,31 +1,33 @@
 const pages = {
 "about-me": ["Hung T Nguyen",
-"<p>I am a 3rd year Computer Science student at Stanford, on the Systems track.</p>" +
-"<p>I've always been interested in computers and technology, ever since I started playing/tinkering with Nintendo DS's and jailbroken iPhones. I believe that computer science can help people on a large scale by solving difficult problems and creating impactful products (and it's fun to do both!) </p>"
+"I am a 3rd year Computer Science student at Stanford, on the Systems track.",
+"I've always been interested in computers and technology, ever since I started playing/tinkering with Nintendo DS's and jailbroken iPhones. I believe that computer science can help people on a large scale by solving difficult problems and creating impactful products (and it's fun to do both!)"
 ],
 
 "experience": ["what I've done",
 "<h2><a href=\"https://crypto.stanford.edu/fidelius/\" target=\"_blank\">Fidelius | Software Engineer Researcher</a>" +
-"<br><a class=\"sub-title\">03.2018 - 07.2018</a></h2>" +
-"<p>Responsibilities<ul><li>Utilized the Intel SGX enclave system to create a trusted display output on untrusted computers</li> <li>Implemented a robust API for transferring data securely to and from the trusted origin server, through the untrusted browser, to the trusted monitor</li></ul></p> <p></p><br>" +
-
+"<br><a class=\"sub-title\">03.2018 - 07.2018</a></h2>",
+"Responsibilities<ul><li>Utilized the Intel SGX enclave system to create a trusted display output on untrusted computers</li> <li>Implemented a robust API for transferring data securely to and from the trusted origin server, through the untrusted browser, to the trusted monitor</li></ul>",
+"",
 "<h2><a href=\"http://www.oneweb.world/\" target=\"_blank\">OneWeb | Software Engineer Intern</a>" +
-"<br><a class=\"sub-title\">06.2017 - 09.2017</a></h2>" +
-"<p>Responsibilities<ul><li>Designed and deployed an AR mobile attitude indicator as a user-facing product for offline, low-power use</li> <li>Led weekly meetings and presentations; collaborated with other teams to deliver metrics</li></ul></p> <p></p><br>" +
-
+"<br><a class=\"sub-title\">06.2017 - 09.2017</a></h2>",
+"Responsibilities<ul><li>Designed and deployed an AR mobile attitude indicator as a user-facing product for offline, low-power use</li> <li>Led weekly meetings and presentations; collaborated with other teams to deliver metrics</li></ul>",
+"",
 "<h2><a href=\"https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=1321586249&mt=8\" target=\"_blank\">FamiliAR | Software Engineer, Designer</a>" +
-"<br><a class=\"sub-title\">11.2017 - 12.2017</a></h2>" +
-"<iframe width=\"336\" height=\"189\" src=\"https://www.youtube-nocookie.com/embed/o0UHfR5vzJI\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>" +
-"<p>Responsibilities<ul><li>Deployed the first Escape Room VR experience on the App Store with a limited time frame</li> <li>Organized and led team meetings when the team lead dropped the project</li> <li>Solved unique spatial AR challenges through mechanics and story</li></ul></p> <p></p><br>"
+"<br><a class=\"sub-title\">11.2017 - 12.2017</a></h2>",
+"<iframe width=\"336\" height=\"189\" src=\"https://www.youtube-nocookie.com/embed/o0UHfR5vzJI\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>",
+"<p>Responsibilities<ul><li>Deployed the first Escape Room VR experience on the App Store with a limited time frame</li> <li>Organized and led team meetings when the team lead dropped the project</li> <li>Solved unique spatial AR challenges through mechanics and story</li></ul><br>",
+""
 ],
 
 "welcome": ["Howdy~",
-"<p>Welcome! My name is Hung, and I'm glad you found this website.  </p><p>Hopefully, you're where you expect to be! Through this site, you can learn a little bit more about me and what I do. </p>" +
-"<p>It's definitely under construction, so there isn't a lot here yet. Do check back periodically in the next couple weeks!</p>" +
-"<br><p>LinkedIn: <a href=\"https://www.linkedin.com/in/hung-the-nguyen\" target=\"_blank\"> https://www.linkedin.com/in/hung-the-nguyen </a></p>" +
-"<p>Github: <a href=\"https://github.com/hnguyen094\" target=\"_blank\"> https://github.com/hnguyen094 </a></p>"
+"Welcome! My name is Hung, and I'm glad you found this website.",
+"Hopefully, you're where you expect to be! Through this site, you can learn a little bit more about me and what I do.",
+"It's definitely under construction, so there isn't a lot here yet. Do check back periodically in the next couple weeks!",
+"LinkedIn: <a href=\"https://www.linkedin.com/in/hung-the-nguyen\" target=\"_blank\"> https://www.linkedin.com/in/hung-the-nguyen </a>",
+"Github: <a href=\"https://github.com/hnguyen094\" target=\"_blank\"> https://github.com/hnguyen094 </a>"
 ],
 
 "contact" : ["contact",
-"<p>You can reach me through email: <a href=\"mailto:hungn2@stanford.edu\">hungn2@stanford.edu</a></p>"
+"You can reach me through email: <a href=\"mailto:hungn2@stanford.edu\">hungn2@stanford.edu</a>"
 ]};
--- js/index.js
+++ js/index.js
@@ -1,36 +1,42 @@
 class App {
-  constructor() {
-    const menu = document.querySelectorAll('.menu-item');
-    const startpage = "welcome";
-    this.openPage(startpage);
+    constructor() {
+        const menu = document.querySelectorAll('.menu-item');
+        const startpage = "welcome";
+        this.openPage(startpage);
 
-    for (let menu_item of menu) {
-      menu_item.addEventListener("mouseover", () => {
-        menu_item.classList.add('hover');
-        menu_item.classList.remove('unhover');
-      });
-      menu_item.addEventListener("mouseout", () => {
-        menu_item.classList.remove('hover');
-        menu_item.classList.add('unhover');
-      });
-      menu_item.addEventListener("click", () => {
-        for (let item of menu) {
-          item.classList.add('unselected');
-          item.classList.remove('selected');
+        for (let menu_item of menu) {
+            menu_item.addEventListener("mouseover", () => {
+                menu_item.classList.add('hover');
+                menu_item.classList.remove('unhover');
+            });
+            menu_item.addEventListener("mouseout", () => {
+                menu_item.classList.remove('hover');
+                menu_item.classList.add('unhover');
+            });
+            menu_item.addEventListener("click", () => {
+                for (let item of menu) {
+                    item.classList.add('unselected');
+                    item.classList.remove('selected');
+                }
+                menu_item.classList.add('selected');
+                menu_item.classList.remove('unselected');
+                this.openPage(menu_item.id);
+            });
         }
-        menu_item.classList.add('selected');
-        menu_item.classList.remove('unselected');
-        this.openPage(menu_item.id);
-      });
     }
-  }
-  openPage(id) {
-    // TODO: put the selected and unselected here.
-    const content = document.querySelector('#content');
-    const title = pages[id][0];
-    content.querySelector('h1').innerHTML = title;
-    content.querySelector('span').innerHTML = pages[id][1];
-  }
+    openPage(id) {
+        // TODO: put the selected and unselected here.
+        const content = document.querySelector('#content');
+        const title = pages[id][0];
+        content.querySelector('h1').innerHTML = title;
+        let body = '';
+        for (let i = 1; i < pages[id].length; i++)
+            body += '<p>' + pages[id][i] + '</p>';
+        content.querySelector('span').innerHTML = this.formatText(body);
+    }
+    formatText(str) {
+        return str.replace(/\n/g, '</p><p>');
+    }
 }
 
 const app = new App();
