b47a0d855b4de7ff941307d8a949ac1f7c3cec8c
Hung Nguyen <hungnguyen094@gmail.com>
2018-11-19 17:36:28 -0800

    updated education and minor changes

--- js/content.js
+++ js/content.js
@@ -1,6 +1,6 @@
 /* Current Features and Limitations
- * \n = <p></p>
- * [text](link) = hyperlink text, but only 1 per string; text optional
+ * \n = <br>, new item = <p>, \t = "    "
+ * [text](link) = hyperlink text; text optional
  * #text ##subtitle= <h2>text <subtitle></h2>, but only 1 subtitle per string
  * */
 const pages = {
@@ -25,18 +25,29 @@ const pages = {
 "welcome": ["~howdy~",
 "Welcome! My name is Hung, and I'm glad you found this website.",
 "Hopefully, you're where you expect to be! Through this site, you can learn a little bit more about me and what I do.",
-"It's definitely under construction, so there isn't a lot here yet. Do check back periodically in the next couple weeks!",
-"LinkedIn: [](https://www.linkedin.com/in/hung-the-nguyen)",
-"Github: [](https://github.com/hnguyen094)"
+"It's definitely under construction, so there isn't a lot here yet. Do check back periodically in the next couple weeks!"
 ],
 
-"education": ["undergraduate education",
-"Below are some of the relevant classes I've taken.",
-"#Systems",
-"[Computer Abstractions in C++ (Accelerated)]{cs106x covers advanced programming topics such as recursion, algorithmic analysis, and data abstraction using the C++ language.}",
-"[Computer Organization and Systems]{cs107 works from the C programming language down to the microprocessor to de-mystify the machine; topics include data representation and optimization of memory and runtime performance.}"
+"education": ["undergrad classes",
+"Below are some of the relevant classes I've taken, organized by their focus.",
+"#Systems Track Courses",
+"[Computer Abstractions in C++ (Accelerated)]{cs106x covers advanced programming topics such as recursion, algorithmic analysis, and data abstraction using the C++ language.}\t"+
+"[Web Programming Fundamentals]{cs193x is and introduction to full-stack web development techniques without the use of any frontend framework.}\t" +
+"[Computer Organization and Systems]{cs107 works from the C programming language down to the microprocessor to de-mystify the machine; topics include data representation and optimization of memory and runtime performance.}\t"+
+"[Principles of Computer Systems]{cs110 covers: overview of the Linux Filesystems; multiprocessing and exceptional control flow; threading and concurrency; networking and distributed computing.}\t" +
+"[Operating Systems and Systems Programming]{cs140 introduces the basic facilities provided in modern operating systems through the Pintos kernel: concurrency (eg. context switching, scheduling); memory management (eg. virtual memory, demand paging); filesystems (eg. directories, crash recovery.)}\t" +
+"[Introduction to Cryptography]{cs255 introduces the basic theory and practice of cryptographic techniques used in computer security. Topics covered include: encryption; cryptographic hashing; Network security protocols (SSL, IPsec); public-key infrastructure.}",
+"#Game Design Courses",
+"[Introduction to Game Design and Development]{cs146 is a project-based course covering topics like 2D/3D Art, Audio, User Interfaces, Production, Narrative Design, Marketing, and Publishing.}\t" +
+"[Virtual People]{comm166 examines digital human representations through the lenses of popular culture (literature & film), engineering (construction), behavioral science (interactions with), computer science (AI), and communication (changing real life behavior).}\t" +
+"[Designing Serious Games (current)]{cs377g is a studio class that provides game design principles to apply to games that teach or promote understanding of complex social and ecological challenges. Learning objectives include iterative development, project scoping, and balancing education with engagement.}",
+"#AI/ML Track Courses",
+"[Intro to Probability for Computer Scientists]{cs109 covers basic concepts including combinatorics, random variables, distributions, expectation, point estimation, and limit theorems. Applications include machine learning and the use of probability in the analysis of algorithms.}\t" +
+"[Mathematical Foundations of Computing]{cs103 is an introduction to discrete mathematics, computability theory, and complexity theory. Topics covered include finite automata, context-free grammar, and Turing machines.}\t" +
+"[AI for Social Good]{cs21si teaches students to apply artificial inteligence techniques such as regression, support vector machines (SVMs), neural networks, CNNs, and RNNs to real-world social good spaces, such as healthcare, goverment, education, and environment.}"
 ],
 
 "contact" : ["contact",
-"You can reach me through email: [hungn2@stanford.edu](mailto:hungn2@stanford.edu)"
+"You can reach me through [email](mailto:hungn2@stanford.edu) or through [LinkedIn](https://www.linkedin.com/in/hung-the-nguyen).",
+"You can also find some of my projects on [Github](https://github.com/hnguyen094) and [Medium](https://medium.com/@hungnguyen094/)."
 ]};
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-class App {
+class Website {
     constructor() {
         const menu = document.querySelectorAll('.menu-item');
         const startpage = "welcome";
@@ -37,55 +37,59 @@ class App {
     }
 
     formatText(str) {
-        let newstr = str.replace(/\n/g, '</p><p>'); // \n as new <p>
-        newstr = newstr.replace(/\[.*\]\(.*\)/g, function(st) { // match []()
+        let newstr = str.replace(/\n/g, '<br>'); // \n as new <br>
+        newstr = newstr.replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;'); // \t
+        let regex = /\[[^\[\]]+\]\([^\(\)]+\)/g;
+        newstr = newstr.replace(regex, function(st) { // match []()
+            console.log(st);
             const dat = this.parsePattern(st, "[]()");
             return this.createHyperlink(dat[0], dat[1]);
         }.bind(this));
-
-        newstr = newstr.replace(/\[.*\]\{.*\}/g, function(st) {
+        regex = /\[[^\[\]]+\]\{[^\{\}]+\}/g;
+        newstr = newstr.replace(regex, function(st) { // match []{}
             const dat = this.parsePattern(st, "[]{}");
             return this.createPopup(dat[0], dat[1]);
         }.bind(this));
 
-        if (newstr[0] == '#') {
+        if (newstr[0] == '#') { // # as <h2>
             newstr = '<h2>' + newstr.substr(1, newstr.length-1) + '</h2>';
             const subIndex = newstr.indexOf('##');
-            if (subIndex > -1) {
+            if (subIndex > -1) { // ## as sub-title to h2
                 const subtitle = newstr.substr(subIndex + 2).trim();
                 newstr = newstr.substr(0, newstr.indexOf('##')) +
-                         '<br><a class=\"sub-title\">' + subtitle + '</a>';
+                    '<br><a class=\"sub-title\">' + subtitle + '</a>';
             }
         }
-        return '<p>' + newstr + '</p>';
+        return '<p>' + newstr+ '</p>';
     }
 
     parsePattern(st, chars) { // such as "[]()"
         const first = st.substr(st.indexOf(chars[0]) + 1, st.indexOf(chars[1]) -
-                                st.indexOf(chars[0]) - 1).trim();
+            st.indexOf(chars[0]) - 1).trim();
         const second= st.substr(st.indexOf(chars[2]) + 1, st.indexOf(chars[3]) -
-                                st.indexOf(chars[2]) - 1).trim();
+            st.indexOf(chars[2]) - 1).trim();
         return [first, second];
     }
 
     createPopup(text, popup) {
-        return '<a class=\"popup\" onmouseleave=\"togglePopup(event)\" ' +
-               'onmouseenter=\"togglePopup(event)\">' +
-               text + ' &#x1F6C8;<span class=\"popuptext\">' + popup + '</span></a>';
+        const fn = '\"website.togglePopup(event)\"';
+        return '<a class=\"popup\" onmouseleave=' +  fn +
+            'onmouseenter='+ fn + '>' +
+            text + ' &#9432;<span class=\"popuptext\">' + popup + '</span></a>';
     }
 
     createHyperlink(txt, url) {
         return '<a href=\"' + url + '\" target=\"_blank\"> ' +
-               (txt.length == 0? url : txt) + '</a>';
+            (txt.length == 0? url : txt) + '</a>';
+    }
+    togglePopup(e) {
+        let popup = "";
+        if (e.target.classList.contains("popup"))
+            popup = e.target.querySelector(".popuptext");
+        else
+            popup = e.target;
+        popup.classList.toggle("show");
     }
 }
 
-function togglePopup(e) {
-    let popup = "";
-    if (e.target.classList.contains("popup"))
-        popup = e.target.querySelector(".popuptext");
-    else
-        popup = e.target;
-    popup.classList.toggle("show");
-}
-const app = new App();
+const website = new Website();
