a33b69d0b1937999e5c72433b69b59f8554e6bc5
Hung Nguyen <hungnguyen094@gmail.com>
2018-12-03 16:29:49 -0800

    tctfy: webkit update

--- storage/tctfy/mods.js
+++ storage/tctfy/mods.js
@@ -7,6 +7,7 @@ class Mods {
         inputField.style.position= "relative";
         inputField.style.width = "50vw";
         inputField.style.animation = "slide 1s";
+        inputField.style.webkitAnimation = "slide 1s";
     }
 }
 const mods = new Mods();
