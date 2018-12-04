68a8eb79000e0ee60f6a9456930d68e4fdd7018f
Hung Nguyen <hungnguyen094@gmail.com>
2018-12-03 17:13:52 -0800

    tctfy: minor font edits

--- storage/tctfy/mods.js
+++ storage/tctfy/mods.js
@@ -1,8 +1,26 @@
 class Mods {
     constructor() {
         document.addEventListener("keypress", this.changeInputStyle);
+        document.addEventListener("click", this.pullKeyboard);
+        document.getScroll = function() {
+            if (window.pageYOffset != undefined) {
+                return [pageXOffset, pageYOffset];
+            } else {
+                var sx, sy, d = document,
+                    r = d.documentElement,
+                    b = d.body;
+                sx = r.scrollLeft || b.scrollLeft || 0;
+                sy = r.scrollTop || b.scrollTop || 0;
+                return [sx, sy];
+            }
+        }
+    }
+    pullKeyboard() {
+        const inputField = document.querySelector('.Input');
+        inputField.focus();
     }
     changeInputStyle() {
+        console.log(document.getScroll());
         const inputField = document.querySelector('.Input');
         inputField.style.position= "relative";
         inputField.style.width = "50vw";
