8ba0d0d25de39033ae2943a9db77c731483791e9
Hung Nguyen <hungnguyen094@gmail.com>
2020-07-03 03:39:47 -0400

    Added additional text + new layout

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,79 @@
-function sortGallery(classList) {
-    if (typeof(classList) != String) {
-        console.log("Error in sortGallery: param is not a string");
-        return;
-    }
+// function sortGallery(classList) {
+//     if (typeof(classList) != String) {
+//         console.log("Error in sortGallery: param is not a string");
+//         return;
+//     }
     
+// }
+
+function initBody(elem) {
+    console.log('initing');
+    const title = elem.getElementsByClassName("item-title")[0];
+    const computedHeight = window.getComputedStyle(title).getPropertyValue("height");
+    const body = elem.getElementsByClassName("item-body")[0];
+    body.style.opacity = 0;
+}
+
+function toggleBody(elem, event = null) {
+    if(event && event.key !== "Enter" && event.key !== ' ') return;
+    if(event) event.preventDefault();
+    const body = elem.getElementsByClassName("item-body")[0];
+    if (body.style.opacity === "0" || body.style.opacity === "") {
+        showBody(elem);
+    } else {
+        const link = elem.getElementsByClassName("link")[0];
+        if (link) window.open(link.textContent, '_blank');
+        hideBody(elem);
+    }
+}
+
+function showBody(elem) {
+    const body = elem.getElementsByClassName("item-body")[0];
+    const title = elem.getElementsByClassName("item-title")[0];
+    const body_overlay = elem.getElementsByClassName("color-body")[0];
+
+    const computedHeight = window.getComputedStyle(title).getPropertyValue("height");
+    title.style.transform = "translateY(" + (parseInt(computedHeight) / 4) + "px)";
+    body.style.transform = "";
+    body.style.opacity = 1;
+    title.style.opacity = 0;
+    body_overlay.style.opacity = 1;
+    body_overlay.style.borderTop = "4vw";
+    body_overlay.style.borderRight = "4vw";
+    // title.style.alignItems = "flex-start";
+
+    const left = elem.getElementsByClassName("left-corner")[0];
+    if (left) {
+        left.style.opacity = 0;
+    }
+    const right = elem.getElementsByClassName("right-corner")[0];
+    if (right) {
+        right.style.opacity = 0;
+    }
+}
+
+function hideBody(elem) {
+    const body = elem.getElementsByClassName("item-body")[0];
+    const title = elem.getElementsByClassName("item-title")[0];
+    const body_overlay = elem.getElementsByClassName("color-body")[0];
+    if (body.style.opacity !== "0") {
+        const computedHeight = window.getComputedStyle(title).getPropertyValue("height");
+        title.style.transform = "";    
+        body.style.transform = "";//"translateY(" + (-parseInt(computedHeight) / 4) + "px)";
+        body.style.opacity = 0;
+        title.style.opacity = "";
+        body_overlay.style.opacity = "";
+        body_overlay.style.borderTop = "";
+        body_overlay.style.borderRight = "";
+        // title.style.alignItems = "";
+    }
+
+    const left = elem.getElementsByClassName("left-corner")[0];
+    if (left) {
+        left.style.opacity = "";
+    }
+    const right = elem.getElementsByClassName("right-corner")[0];
+    if (right) {
+        right.style.opacity = "";
+    }
 }
